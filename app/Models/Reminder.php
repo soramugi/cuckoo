c134ff383031c508241d606f97b106a0aae04587
soramugi <soramugi.chika@gmail.com>
2023-10-26 00:37:35 +0900

    一覧表示での繰り返し期間の表示に対応

--- app/Models/Reminder.php
+++ app/Models/Reminder.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -23,4 +24,20 @@ class Reminder extends Model
 
         'compleded_at',
     ];
+
+    protected function repeatText(): Attribute
+    {
+        return Attribute::make(
+            get: function (mixed $value, array $attributes) {
+                if (!is_null($attributes['week'])) {
+                    $weeks = ['日', '月', '火', '水', '木', '金', '土'];
+                    return '毎週' . $weeks[$attributes['week']] . '曜日';
+                }
+                if (!is_null($attributes['day'])) {
+                    return '毎月' . $attributes['day'] . '日';
+                }
+                return $attributes['day'];
+            },
+        );
+    }
 }
--- resources/views/reminders/index.blade.php
+++ resources/views/reminders/index.blade.php
@@ -36,7 +36,7 @@
                         @foreach ($reminders as $reminder)
                         <tr>
                             <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
-                                {{ $reminder->repeatText }}
+                                {{ $reminder->repeat_text }}
                             </td>
                             <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
                                 {{ $reminder->time }}
