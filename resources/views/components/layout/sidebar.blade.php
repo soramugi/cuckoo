d801d1147632521731dd5de42fec11e8ef755735
soramugi <soramugi.chika@gmail.com>
2023-11-11 17:53:52 +0900

    使い方として公式ドキュメントのページに誘導

--- resources/views/components/layout/sidebar.blade.php
+++ resources/views/components/layout/sidebar.blade.php
@@ -56,6 +56,15 @@
                     </svg>
                     Settings
                 </a>
+                <a href="https://cuckooremind.com"
+                    class="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-700 hover:text-indigo-600 hover:bg-gray-50">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-6 h-6 shrink-0 text-gray-400 group-hover:text-indigo-600">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z" />
+                    </svg>
+                    使い方
+                </a>
             </li>
         </ul>
     </nav>
